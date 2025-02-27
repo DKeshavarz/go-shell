9bc0f5ed813cc9ee74ae3c1e980416c762c2d6ea
DKeshavarz <dankeshavarz1075@gmail.com>
2025-02-28 02:48:07 +0330

    Chore : Build Database and connection

---
+++ internal/storage/db.go
@@ -0,0 +1,42 @@
+package database
+
+import (
+	"fmt"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+const (
+	dbHost = "localhost"
+	dbPort = "5432"
+	dbName = "go_shell"
+	dbUser = "postgres"
+	dbPass = "password"
+)
+
+
+var db *gorm.DB
+
+func init() {
+	fmt.Println("========================================================================================================================")
+	fmt.Println("connecting to database...")
+
+	dsn := fmt.Sprintf("host=%s port=%s dbname=%s user=%s password=%s sslmode=disable",
+		dbHost, dbPort, dbName, dbUser, dbPass)
+
+	sesssion, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		err = fmt.Errorf("unable to connect to database: %w", err)
+		panic(err)
+	}
+
+	db = sesssion
+
+	fmt.Printf("connected to database: %s\n", dsn)
+	fmt.Println("========================================================================================================================")
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
---
+++ internal/storage/go_shell.session.sql
@@ -0,0 +1,16 @@
+-- CREATE TABLE users (
+--     username VARCHAR(255) PRIMARY KEY,
+--     password VARCHAR(255) NOT NULL
+-- );
+
+-- CREATE TABLE commands (
+--     username   VARCHAR(255),
+--     command    VARCHAR(255),
+--     created_at TIMESTAMP NOT NULL,
+--     count      INT,
+--     PRIMARY KEY(username,command),
+--     FOREIGN KEY(username) REFERENCES users (username) ON UPDATE CASCADE ON DELETE CASCADE
+-- );
+
+
+
